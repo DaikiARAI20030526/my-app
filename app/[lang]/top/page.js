e3b67781e9c6f527e758e1d2277694b825ec1723
DaikiARAI20030526 <araidaiki2003@gmail.com>
2025-09-28 08:20:34 +0900

    top変更

--- app/[lang]/top/page.js
+++ app/[lang]/top/page.js
@@ -1,16 +1,73 @@
-import Toptitle from "../components/toptitle/toptitle"
-import Navigation from "../components/navigation/navigation"
-import TopBord from "../components/top_bord/top_bord"
-import Newscardlist from "../components/newscardlist/newscardlist"
-import ProjectCardList from "../components/projectcardlist/projectcardlist"
-import TopPageContent from "../components/TopPageContent/TopPageContent"
-import InternalNavigation from "../components/InternalNavigation/InternalNavigation"
-import ResearchStickyHeader from "../components/ResearchStickyHeader/ResearchStickyHeader"
-import Parser from "rss-parser"
+import Toptitle from "../../components/toptitle/toptitle";
+import Navigation from "../../components/navigation/navigation";
+import TopBord from "../../components/top_bord/top_bord";
+import Newscardlist from "../../components/newscardlist/newscardlist";
+import ProjectCardList from "../../components/projectcardlist/projectcardlist";
+import TopPageContent from "../../components/TopPageContent/TopPageContent";
+import InternalNavigation from "../../components/InternalNavigation/InternalNavigation";
+import ResearchStickyHeader from "../../components/ResearchStickyHeader/ResearchStickyHeader";
+import Parser from "rss-parser";
 
-export const revalidate = 60
+// 1. 日本語と英語のテキストをオブジェクトとして定義
+const content = {
+  jp: {
+    pageTitles: [
+      "シチズンサイエンスについて / 研究業績 / 研究メンバーについて",
+      "お知らせ",
+      "現在進行中のプロジェクト",
+    ],
+    nav1_copy: "はじめに",
+    nav1_name: "シチズンサイエンスについて",
+    nav1_explanation: "シチズンサイエンスってそもそもなんなのかと、ご説明します",
+    nav1_slug: "about",
+    nav2_copy: "つぎに",
+    nav2_name: "研究業績",
+    nav2_explanation: "論文、学会発表、書籍など当センターの研究業績のアーカイブをご覧いただけます。",
+    nav2_slug: "research",
+    nav3_copy: "それでは",
+    nav3_name: "研究メンバーについて",
+    nav3_explanation: `研究所に現在在籍するメンバーについてご紹介します。\nまた、研究メンバーの募集についてもこちらに記載しています。`,
+    nav3_slug: "member",
+    news_title: "お知らせ",
+    projects_title: "現在進行中のプロジェクト",
+    note_link: "noteへ",
+    news_keyword: "お知らせ",
+    project_keyword: "project",
 
-export default async function TopPage() {
+  },
+  en: {
+    pageTitles: [
+      "About Citizen Science / Research / Members",
+      "News",
+      "Ongoing Projects",
+    ],
+    nav1_copy: "To begin with",
+    nav1_name: "About Citizen Science",
+    nav1_explanation: "We will explain what Citizen Science is all about.",
+    nav1_slug: "about",
+    nav2_copy: "Next",
+    nav2_name: "Research Achievements",
+    nav2_explanation: "You can view the archive of our center's research achievements, including papers, conference presentations, and books.",
+    nav2_slug: "research",
+    nav3_copy: "Finally",
+    nav3_name: "About Research Members",
+    nav3_explanation: "Introducing the current members of the research institute.\nRecruitment for research members is also posted here.",
+    nav3_slug: "member",
+    news_title: "News",
+    projects_title: "Ongoing Projects",
+    note_link: "Go to note",
+    news_keyword: "Notice", // noteの記事タイトルに含まれる英語キーワードを想定
+    project_keyword: "project",
+  },
+};
+
+export const revalidate = 60;
+
+// 2. ページのpropsで `params` を受け取り、`lang` を取得
+export default async function TopPage({ params: { lang } }) {
+  // 3. URLの言語に応じて、表示するテキストを選択（不正な場合は 'jp' にフォールバック）
+  const t = content[lang] || content.jp;
+
   const parser = new Parser({
     customFields: {
       item: [
@@ -18,38 +75,35 @@ export default async function TopPage() {
         ["media:thumbnail", "thumbnail"],
       ],
     },
-  })
+  });
 
-  const feed = await parser.parseURL("https://note.com/csrc/rss")
+  const feed = await parser.parseURL("https://note.com/csrc/rss");
 
+  // 4. RSSフィードのフィルタリングも動的に変更
   const articles = feed.items
-    .filter((item) => item.title?.includes("お知らせ"))
+    .filter((item) => item.title?.includes(t.news_keyword))
     .map((item) => ({
-      date: item.pubDate ?? "日付不明",
+      date: item.pubDate ?? "Date Unknown",
       title: item.title,
       link: item.link,
-    }))
+    }));
 
   const projectArticles = feed.items
-    .filter((item) => item.title?.includes("project"))
+    .filter((item) => item.title?.includes(t.project_keyword))
     .map((item) => ({
-      date: item.pubDate ?? "日付不明",
+      date: item.pubDate ?? "Date Unknown",
       title: item.title,
       thumbnail: item.thumbnail || "/sample_project.jpg",
       creatorImage: item.creatorImage || "/default_user.jpg",
       link: item.link,
-    }))
+    }));
 
   return (
     <main>
       <Toptitle />
       <InternalNavigation />
       <ResearchStickyHeader
-        pageTitles={[
-          "シチズンサイエンスについて　/　研究業績　/　研究メンバーについて",
-          "お知らせ",
-          "現在進行中のプロジェクト",
-        ]}
+        pageTitles={t.pageTitles}
         subtitleIds={["subtitle0", "subtitle1", "subtitle2"]}
       />
       <div className="heroSection">
@@ -58,43 +112,43 @@ export default async function TopPage() {
         </div>
       </div>
 
+      {/* 5. Navigationコンポーネントのリンク先と言語を動的に設定 */}
       <Navigation
-        navicopy="はじめに"
-        naviname="シチズンサイエンスについて"
-        naviexplanation="シチズンサイエンスってそもそもなんなのかと、ご説明します"
-        link="/about"
+        navicopy={t.nav1_copy}
+        naviname={t.nav1_name}
+        naviexplanation={t.nav1_explanation}
+        link={`/${lang}/${t.nav1_slug}`}
         imagePath="/go 1 a.png"
         imageHoverPath="/go 1 b.png"
       />
 
       <Navigation
-        navicopy="つぎに"
-        naviname="研究業績"
-        naviexplanation="論文、学会発表、書籍など当センターの研究業績のアーカイブをご覧いただけます。"
-        link="/research"
+        navicopy={t.nav2_copy}
+        naviname={t.nav2_name}
+        naviexplanation={t.nav2_explanation}
+        link={`/${lang}/${t.nav2_slug}`}
         imagePath="/go 1 a.png"
         imageHoverPath="/go 1 b.png"
       />
 
       <Navigation
-        navicopy="それでは"
-        naviname="研究メンバーについて"
-        naviexplanation={`研究所に現在在籍するメンバーについてご紹介します。
-また、研究メンバーの募集についてもこちらに記載しています。`}
-        link="/member"
+        navicopy={t.nav3_copy}
+        naviname={t.nav3_name}
+        naviexplanation={t.nav3_explanation}
+        link={`/${lang}/${t.nav3_slug}`}
         imagePath="/go 3 a.png"
         imageHoverPath="/go 3 b.png"
       />
 
       <div id="subtitle1" className="anchorSection">
-        <TopBord bordname="お知らせ" notelink="noteへ" />
+        <TopBord bordname={t.news_title} notelink={t.note_link} />
       </div>
       <Newscardlist data={articles} />
 
       <div id="subtitle2" className="anchorSection">
-        <TopBord bordname="現在進行中のプロジェクト" notelink="noteへ" />
+        <TopBord bordname={t.projects_title} notelink={t.note_link} />
       </div>
       <ProjectCardList data={projectArticles} />
     </main>
-  )
+  );
 }
