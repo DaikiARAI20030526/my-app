20dc54ab2581dbe234c288897e34f2eda926568b
DaikiARAI20030526 <araidaiki2003@gmail.com>
2026-02-12 11:36:51 +0900

    リダイレクト処理

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  /* config options here */
-  // すでに存在する他のオプションがあればそれらはそのまま残してください
+  
+  trailingSlash: false, 
+
   images: {
-    // 以下のどちらかを使う
-
-    // 1. domains で指定する場合
     domains: ['images.microcms-assets.io'],
-
-    // 2. remotePatterns を使う場合 (Next.js 13+)
-    /*
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
-        port: '',
-        pathname: '/assets/**',
-      },
-    ],
-    */
+    
   },
 };
 
 export default nextConfig;
-
