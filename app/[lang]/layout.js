24e707a53a895e5931e3f23107129fb6731b4888
DaikiARAI20030526 <araidaiki2003@gmail.com>
2026-02-12 11:24:12 +0900

    metaタグ追加

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -1,3 +1,10 @@
+export const metadata = {
+  verification: {
+    google: '4s1VZp3G7MoyeTwlciHcLHltqZr5D0XnqNO1Jwl-7PE', 
+  },
+};
+
+// 元のコード（そのまま）
 export default function TopLayout({ children }) {
   return (
     <>
